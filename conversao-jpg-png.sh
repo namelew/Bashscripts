028af1c56abea9c4baa054e2319b27a8a659d8ce
namelew <diogomaciel.cunha@gmail.com>
2023-04-11 10:55:17 -0300

    removing extension and redicting files to images-livros/png

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -4,7 +4,13 @@ CAMINHO_IMAGENS=imagens-livros
 
 cd $CAMINHO_IMAGENS
 
+if [ ! -d png ]
+then
+    mkdir png
+fi
+
 for imagem in *.jpg
 do
-    convert $imagem $imagem.png
+    imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
